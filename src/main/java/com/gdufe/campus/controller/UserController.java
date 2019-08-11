f1e7f98fc5a5b47107ab6399afd28c4bf5ad3d0d
bravery <2472937751@qq.com>
2019-08-11 10:25:43 +0800

    userController view层打通

--- src/main/java/com/gdufe/campus/controller/UserController.java
+++ src/main/java/com/gdufe/campus/controller/UserController.java
@@ -1,4 +1,36 @@
 package com.gdufe.campus.controller;
 
+import com.gdufe.campus.pojo.DO.UserDO;
+import com.gdufe.campus.pojo.DTO.UserDTO;
+import com.gdufe.campus.pojo.VO.UserVO;
+import com.gdufe.campus.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
+@RequestMapping("/user")
+@Slf4j
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/login")
+    public String login(Model model) {
+        return "user/login";
+
+    }
+    @PostMapping("/login")
+    public String login1(@RequestBody UserVO userVO) {
+
+        UserDTO userDTO = new UserDTO();
+        userDTO.setAccount(userVO.getAccount());
+        userDTO.setPassword(userVO.getPassword());
+        userService.login(userDTO);
+        return "user/login";
+
+    }
 }
---
+++ src/main/java/com/gdufe/campus/pojo/VO/UserVO.java
@@ -0,0 +1,21 @@
+package com.gdufe.campus.pojo.VO;
+
+import lombok.Data;
+
+@Data
+public class UserVO {
+
+    private Long id;
+
+    private String account;
+
+    private String password;
+
+    private String username;
+
+    private String email;
+
+    private String headImg;
+
+
+}
