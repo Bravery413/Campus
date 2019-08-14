382c9cd936f7682d1ba20cde95cf431f3fb27a28
py <123456@qq.com>
2019-08-14 15:49:29 +0800

    纯前端验证码功能实现

--- src/main/java/com/gdufe/campus/controller/CaptchaController.java
+++ src/main/java/com/gdufe/campus/controller/CaptchaController.java
@@ -3,6 +3,7 @@ package com.gdufe.campus.controller;
 
 import com.wf.captcha.utils.CaptchaUtil;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,6 +16,11 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 @RequestMapping("/captcha")
 public class CaptchaController {
+
+    @GetMapping("/codePage")
+    public String codePage() {
+        return "/test/code";
+    }
 
     @RequestMapping("/image")
     public void captcha(HttpServletRequest request, HttpServletResponse response) throws Exception {
