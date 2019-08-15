df8cf3a2dac4d1cbc9875ecf08590a5381e19296
bravery <2472937751@qq.com>
2019-08-16 00:07:42 +0800

    页面重整完毕

--- src/main/java/com/gdufe/campus/controller/BaseController.java
+++ src/main/java/com/gdufe/campus/controller/BaseController.java
@@ -3,7 +3,9 @@ package com.gdufe.campus.controller;
 
 import com.gdufe.campus.mapper.CategoryMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
  * @author bravery
  * @date 2019/8/9 10:00
  */
-@RestController
+@Controller
 public class BaseController {
+
+    @GetMapping("/test/login")
+    public String test() {
+        return "test/login";
+    }
 
 }
