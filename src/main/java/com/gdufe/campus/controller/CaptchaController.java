2a40fc948abd3cc233073a61fffe8cbf5e3afd71
bravery <2472937751@qq.com>
2019-08-18 19:45:01 +0800

    修改验证码参数

--- src/main/java/com/gdufe/campus/controller/CaptchaController.java
+++ src/main/java/com/gdufe/campus/controller/CaptchaController.java
@@ -1,12 +1,15 @@
 package com.gdufe.campus.controller;
 
 
+import com.wf.captcha.Captcha;
+import com.wf.captcha.GifCaptcha;
 import com.wf.captcha.utils.CaptchaUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.awt.*;
 
 
 /**
@@ -24,7 +27,14 @@ public class CaptchaController {
 
     @RequestMapping("/image")
     public void captcha(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        CaptchaUtil.out(request, response);
+        //详细设置参数
+        CaptchaUtil.setHeader(response);
+        GifCaptcha gifCaptcha = new GifCaptcha(130, 48, 5);
+        gifCaptcha.setCharType(Captcha.TYPE_DEFAULT);
+        request.getSession().setAttribute("captcha",gifCaptcha.text().toLowerCase());
+        gifCaptcha.out(response.getOutputStream());
+        //简写
+//        CaptchaUtil.out(request, response);
     }
 
 
